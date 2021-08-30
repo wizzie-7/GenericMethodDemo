a293733c64e7159627cc68d1c5c8b2f65d455cde
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-30 18:42:20 +0530

    Program to Implemet Generic Method

---
+++ GnericMethodEx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericMethodDemo
+{
+    class MethodGeneric
+    {
+        public void GenMethod()
+        {
+            List<string> LT = new List<string>();
+            LT.Add("Vivek");
+            LT.Add("Krunal");
+            LT.Add("Nirbhay");
+            LT.Add("Abhi");
+            ProssedItem<string>(LT);
+        }
+        public void ProssedItem<T>(List<T> lst)
+        {
+            foreach (T Item in lst)
+            {
+                Console.WriteLine(Item);
+            }
+
+        }
+    }
+    class GnericMethodEx
+    {
+        static void Main(string[] args)
+        {
+            MethodGeneric mg = new MethodGeneric();
+            mg.GenMethod();
+        }
+    }
+}
